9e89ad783f739bd245d0fd2c0aaa0b1dc9e60412
batuh <batuh@DESKTOP-TT083QO>
2022-02-18 15:43:57 +0300

    Add project files.

---
+++ Oop2/Individual.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop2
+{
+    class Individual:Customer // Gerçek Müşteri
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string TcNo { get; set; }
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Individual customer1 = new Individual();
+            customer1.CustomerNo = "12345";
+            customer1.Id = 1;
+            customer1.Name = "Batuhan";
+            customer1.SurName = "Kaynarcalidan";
+            customer1.TcNo = "123124124123";
+
+            Coorporate customer2 = new Coorporate();
+            customer2.CustomerNo = "12345";
+            customer2.Id = 1;
+            customer2.CompanyName = "BatuhanKaynarcalidan";
+            customer2.TaxNo = "12312312";
+
+            Customer customer3 = new Individual();
+            Customer customer4 = new Coorporate();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+        }
+    }
+}
